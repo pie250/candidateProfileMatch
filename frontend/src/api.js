222a7feb9cce95b67837e4c4d3c3548c00ad7c76
Rohit <parasharohit250@gmail.com>
2026-05-16 17:33:56 +0530

    not

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://candidateprofilematch-1.onrender.com/api",
+  baseURL: "https://candidateprofilematch.onrender.com/api",
 });
 
 export default API;
